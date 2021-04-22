3c94c732b84a18a1bbc1b57722077a392e52f795
Keon Choi <keonch91@gmail.com>
2021-04-22 05:46:07 -0400

    api test

--- index.js
+++ index.js
@@ -1,14 +1,29 @@
 const express = require('express');
 const path = require('path');
 const app = express();
+const https = require('https');
 
 app.use(express.static(path.join(__dirname, 'client/build')));
 
 app.get('/api/fetch_gtfs_feed', (req, res) => {
-    const a = process.env.mta_datamine;
-    res.json(['hey world', 'hello wrld', a]);
-
+    const response = https.get(
+        "https://api-endpoint.mta.info/Dataservice/mtagtfsfeeds/nyct%2Fgtfs-ace",
+        {
+            headers: { "x-api-key": process.env.mta_datamine }
+        },
+        (resp) => {
+            resp.on('data', (chunk) => {
+                console.log("Receiving Data");
+            });
+            resp.on('end', () => {
+                console.log("Finished receiving data");
+            });
+        }).on("error", (err) => {
+            console.log("Error: " + err.message);
+        });
+    res.send(response);
 });
+
 
 app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname + '/client/build/index.html'));
