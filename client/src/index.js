316fb80fe31e2ce9e81dcaaa44283f9dbef33a14
Keon Choi <keonch91@gmail.com>
2021-04-24 22:29:14 -0400

    update file name for herkou

--- client/src/index.js
+++ client/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './assets/stylesheets/index.css';
-import App from './components/App-Container';
+import App from './components/App_container.js';
 import configureStore from './store/store';
 import { Provider } from 'react-redux';
 
